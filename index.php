491bb9c7bb3d38d11cbe05bff019cce2cbc568ed
christina1024 <christinali1024@gmail.com>
2019-04-04 12:56:55 -0600

    new index page

--- index.php
+++ index.php
@@ -3,49 +3,20 @@
 <head>
 	<title>User Login</title>	
 	<style>
-		form{padding-top: 120px;
-			text-align: center;
-			front-size: 30px;}
+		
 			
-		input[type=text]{width: 200px;
-			height: 40px;
-			front-size: 30px;}
-			
-		input[type=password]{width: 200px;
-			height: 40px;
-			front-size: 30px;}
-			
-		input[name=login]{
-			width: 150px;
-			position: absolute;
-			left: 530px;
-			top: 354px;
-			height: 20px;
-			front-size: 15px;}		
-			
-		input[name=register]{
-			width: 150px;
-			position: absolute;
-			left: 720px;
-			top: 354px;
-			height: 20px;
-			front-size: 15px;}	
-			
-		body {
-  background-image: url("images.jpg");
   
-} 
+
 			
 	</style>
 	
 </head>
 <body>
 
-<form action="login.php" method="post">
-	<h1>User Login Page</h1><br>
-   Username: <input type="text" name="username"><br><br>
-   Password: <input type="password" name="password"><br><br>
+	<h1>Some Fancy System name</h1><br>
 
+<form action="loginPage.php" method="post">
+	  
    <input type="submit" name= "login" value="login">
 </form>
 
