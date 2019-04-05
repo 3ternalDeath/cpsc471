90d435085da223fe9fb4b03994c2365c90f21c11
Lynnnnaa <lxsxujiarong@outlook.com>
2019-04-05 00:03:34 -0600

    continue working...

--- index.php
+++ index.php
@@ -1,28 +1,110 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>User Login</title>	
-	<style>
-		
-			
-  
+	<title>Cinema</title>
 
-			
-	</style>
-	
-</head>
+	<style>
+		body {background-image: url("images.jpg");}
+
+		*{box-sizing: border-box;}
+
+		.header, .footer{
+			background-color: #364247;
+			color: white;
+			padding: 15px;
+		}
+
+		.topmenu{
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+			overflow: hidden;
+			position: -webkit-sticky;
+			position: sticky;
+			top: 0;
+			background-color: #5bcfef;
+		}
+
+		.topmenu li{float: left;}
+
+		.topmenu li a, .dropbtn {
+			display: inline-block;
+			color: white;
+			text-align: center;
+			padding: 15px 25px;
+			text-decoration: none;
+		}
+
+		.topmenu li a:hover, .dropdown:hover .dropbtn {background-color: #3d97af}
+
+		.active {background-color: #3d97af}
+
+
+		li.dropdown {
+		  display: inline-block;
+		}
+
+		.dropdown-content {
+		  display: none;
+		  position: absolute;
+		  background-color: #f9f9f9;
+		  min-width: 160px;
+		  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+		  z-index: 1;
+		}
+
+		.dropdown-content a {
+		  color: black;
+		  padding: 12px 16px;
+		  text-decoration: none;
+		  display: block;
+		  text-align: left;
+		}
+
+		.dropdown-content a:hover {background-color: #f1f1f1}
+
+		.dropdown:hover .dropdown-content {
+		  display: block;
+		}
+
+
+	</style></head>
 <body>
 
-	<h1>Some Fancy System name</h1><br>
+<div class="header">
+	<h1>Fancy Cinema</h1><br>
+</div>
 
-<form action="loginPage.php" method="post">
-	  
-   <input type="submit" name= "login" value="login">
-</form>
+<div>
+	<ul class="topmenu">
+		<li><a href="#showTime" class="active">Show Time</a></li>
+		<li class="dropdown">
+			<a href="javascript:void(0)" class="dropbtn">Movie</a>
+			<div class="dropdown-content">
+				<a href="#">Moive</a>
+				<a href="#">Genre</a>
+				<a href="#">Actor</a>
+			</div>
+		</li>
+		<li><a href="#food">Food</a></li>
+		<li><a href="#about">About</a></li>
+		<li style="float:right"><a href="#login">Login</a></li>
+	</ul>
+</div>
 
-<form action="register.php" method="post">
-    <input type="submit" name= "register" value="register" />
-</form>
+<div>
+	<form action="loginPage.php" method="post">
+	   <input type="submit" name= "login" value="login">
+	</form>
+
+	<form action="register.php" method="post">
+	    <input type="submit" name= "register" value="register" />
+	</form>
+</div>
+
+<div class="footer">
+	<p>Hi, my name is cinema</p><br>
+</div>
 
 </body>
 </html>
