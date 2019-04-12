b997a735a5d0e73d90d38d99741b7794f0e3cd49
3ternalDeath <abmisawesome@gmail.com>
2019-04-12 01:16:43 -0600

    made the drop down menue in search by movie better

--- searchByMovieName.php
+++ searchByMovieName.php
@@ -1,5 +1,13 @@
 <?php
 include("indexBase.php");
+// Create connection
+$con=mysqli_connect("localhost","root","","cinemaDB");
+
+// Check connection
+if (mysqli_connect_errno($con))
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 ?>
 <!DOCTYPE html>
 
@@ -11,52 +19,48 @@ include("indexBase.php");
 
 	Genre:	<select name="Genre">
 		<option value="-">ANY</option>
-		<option value="romance">romance</option>
-		<option value="adventure">adventure</option>
-		<option value="action">action</option>
-		<option value="sci-fi">sci-fi</option>
-		<option value="comedy">comedy</option>
-		<option value="documentary">documentary</option>
-	</select> 
+    <?php
+    $sql =  "SELECT DISTINCT genre FROM Genre";
+    $result = mysqli_query($con,$sql);
+    while($row = mysqli_fetch_assoc($result)) {
+      echo "<option value='".$row['genre']."'>";
+      echo $row['genre'];
+      echo "</option>";
+    }
+    ?>
+	</select>
 
   actor: <input type="text" name="actor">
   <input type="submit" value='Search'>
 </form>
 
 <?php
-  // Create connection
-  $con=mysqli_connect("localhost","root","","cinemaDB");
 
-  // Check connection
-  if (mysqli_connect_errno($con))
-  {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$actor = "%".$_POST['actor']."%";		
+		$actor = "%".$_POST['actor']."%";
 		$filer = "%".$_POST['Mname']."%";
 		$Genre = $_POST['Genre'];
    if($Genre=="-"){
 	   $prep = mysqli_prepare($con,"SELECT IMDBID, name ,image FROM Movie as M WHERE EXISTS (SELECT * FROM ActIn as I WHERE EXISTS(SELECT * FROM Actor as A WHERE A.name LIKE? AND M.name LIKE ? AND A.IMDBID=I.actorIMDB AND I.movieIMDB=M.IMDBID))");
 		if ( !$prep ) {
 		die('mysqli error: '.mysqli_error($con));
-		}	
+		}
 		mysqli_stmt_bind_param($prep, "ss",$actor, $filer);
 	}
 	else{
-    $prep = mysqli_prepare($con,"SELECT IMDBID, name, image From Movie as M WHERE EXISTS (SELECT * FROM Genre as G WHERE EXISTS (SELECT * FROM ActIn as I WHERE EXISTS(SELECT * FROM Actor as A WHERE A.name LIKE ? AND G.genre LIKE ? 
+    $prep = mysqli_prepare($con,"SELECT IMDBID, name, image From Movie as M WHERE EXISTS (SELECT * FROM Genre as G WHERE EXISTS (SELECT * FROM ActIn as I WHERE EXISTS(SELECT * FROM Actor as A WHERE A.name LIKE ? AND G.genre LIKE ?
 	AND M.name LIKE ? AND A.IMDBID=I.actorIMDB AND I.movieIMDB=G.movieIMDB AND G.movieIMDB=M.IMDBID)))");
 		mysqli_stmt_bind_param($prep, "sss",$actor,$Genre, $filer);
 	}
-		
+
 	mysqli_stmt_execute($prep);
     $result = mysqli_stmt_get_result($prep);
     $count = mysqli_num_rows($result);
     echo $count." results:";
 	echo '<div style="padding: 20px 0px" >';
     while($row = mysqli_fetch_assoc($result)){
-      echo "<a href='displayMovie.php?movieIMDBID=".$row["IMDBID"]."'><img src=".$row["image"]." height='300' width='210'/></a> "; 
-		echo str_repeat('&nbsp;', 10);		
+      echo "<a href='displayMovie.php?movieIMDBID=".$row["IMDBID"]."'><img src=".$row["image"]." height='300' width='210'/></a> ";
+		echo str_repeat('&nbsp;', 10);
 		}
  }
   if (isset($_GET['Addr'])){
@@ -70,8 +74,8 @@ include("indexBase.php");
     echo $count." movies play at this location: ";
 	echo '<div style="padding: 20px 0px" >';
     while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-		echo "<a href='getShowTime.php?movieIMDBID=".$row["IMDBID"]."& movieLoc=".$_GET['Addr']."'><img src=".$row["image"]." height='300' width='210'/></a> "; 
-		echo str_repeat('&nbsp;', 10);		
+		echo "<a href='getShowTime.php?movieIMDBID=".$row["IMDBID"]."& movieLoc=".$_GET['Addr']."'><img src=".$row["image"]." height='300' width='210'/></a> ";
+		echo str_repeat('&nbsp;', 10);
 		}
   }
 
@@ -84,9 +88,9 @@ include("indexBase.php");
     $count = mysqli_num_rows($result);
     echo "<h1>We have ". $count." movies:</h1>";
 	echo '<div style="padding: 20px 0px" >';
-    while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){	 
-		echo "<a href='displayMovie.php?movieIMDBID=".$row["IMDBID"]."'><img src=".$row["image"]." height='300' width='210'/></a> "; 
-		echo str_repeat('&nbsp;', 10);		
+    while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+		echo "<a href='displayMovie.php?movieIMDBID=".$row["IMDBID"]."'><img src=".$row["image"]." height='300' width='210'/></a> ";
+		echo str_repeat('&nbsp;', 10);
 	}
   }
 
